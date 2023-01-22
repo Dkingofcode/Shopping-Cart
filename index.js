fd8ae0f20c19022e53c372290a81558843fb553d
Dkingofcode <doladepo128@gmail.com>
2023-01-22 19:38:17 +0000

    Added data in products.json

--- index.js
+++ index.js
@@ -6,18 +6,42 @@ const clearCarBtn = document.querySelector(".clear-cart");
 const cartDOM = document.querySelector(".cart")
 const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
-const cartTotal = document.querySelector(".")
-const cartContent = document.querySelector(".");
+const cartTotal = document.querySelector(".cart-total")
+const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".products-center");
 
 // cart 
 let cart = [];
 
 
+// getting products
+class Products {
+  async getProducts(){
+    try{
+    let result =  await fetch('products.json');
+    data =   await result.json();
+    return data;
+    } catch(error){
+       console.log(error);
+    }
+  }  
+}
+
+// display products
+class UI {
+
+}
 
 
+// local storage
+class Storage {
 
+}
 
+document.addEventListener("DOMContentLoaded", () => {
+    const ui =new UI();
+    const products = new Products();
+})
 
 
 
