94f838261d6c550629bd1bf90830138e65d5f419
Dkingofcode <doladepo128@gmail.com>
2023-01-22 16:26:07 +0000

    Finished writing the styles moving to javscript

---
+++ index.js
@@ -0,0 +1,67 @@
+// variables
+
+const carBtn = document.querySelector('.cart-btn')
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCarBtn = document.querySelector(".clear-cart"); 
+const cartDOM = document.querySelector(".cart")
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".")
+const cartContent = document.querySelector(".");
+const productsDOM = document.querySelector(".products-center");
+
+// cart 
+let cart = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
